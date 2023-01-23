92c56f579fa92ab400be63e9dfac8536a0d0b4e3
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2023-01-24 01:41:35 +0530

    feat: add site hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,9 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
