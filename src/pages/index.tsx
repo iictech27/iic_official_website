3d49b1b5bc076cbda0a1b72b97793f761b6421f7
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2023-01-24 02:10:53 +0530

    feat(index): add supported by section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,27 @@
 import Hero from "@/components/Hero";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      {
+        // Supported by section
+      }
+      <div className="flex flex-col pt-28 sm:pt-40">
+        <h4 className="pb-10 text-center text-sm font-bold uppercase tracking-widest text-zinc-600">
+          Supported by
+        </h4>
+        <div className="mx-auto justify-center">
+          <Image
+            src="/images/supportedby.png"
+            alt="IIC TMSL"
+            width={600}
+            height={100}
+            className="object-contain"
+          />
+        </div>
+      </div>
     </>
   );
 }
