33b8297735030995f1db753869f09a06b5c2489c
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2023-01-24 03:24:43 +0530

    feat(core): create Title component

---
+++ src/components/core/Title.tsx
@@ -0,0 +1,11 @@
+import { TitleProps } from "@/types/title";
+
+export default function Title({ title }: TitleProps) {
+  return (
+    <div className="pt-20 pb-40">
+      <h1 className="flex flex-col justify-center text-center text-5xl font-[800] tracking-tighter sm:flex-row sm:gap-x-3 sm:text-7xl">
+        <span className="text-black">{title}</span>
+      </h1>
+    </div>
+  );
+}
