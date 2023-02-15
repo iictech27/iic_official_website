6c2b852012e47f5a0dc1a4acd25623a338417a6b
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2023-02-15 21:37:05 +0530

    feat(layout): add fade in animation

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,3 +1,4 @@
+import FadeIn from "@/components/animation/FadeIn";
 import Footer from "@/components/layout/Footer";
 import Navbar from "@/components/layout/Navbar";
 import MadeBy from "@/components/MadeBy";
@@ -5,7 +6,7 @@ import ScrollToTop from "@/components/ScrollToTop";
 
 export default function Layout({ children }: any): JSX.Element {
   return (
-    <div className="isolate bg-white dark:bg-black">
+    <FadeIn className="isolate bg-white dark:bg-black" noVertical>
       <Navbar />
       <main className="relative px-6 lg:px-8">
         <div className="mx-auto max-w-6xl py-20 sm:pt-28 sm:pb-40">
@@ -15,6 +16,6 @@ export default function Layout({ children }: any): JSX.Element {
       <Footer />
       <ScrollToTop />
       <MadeBy />
-    </div>
+    </FadeIn>
   );
 }
