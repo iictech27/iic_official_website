a5ae0a81fe5c3aaf11c286083e61856da4b89157
Utsav <utsavtiwari030@outlook.com>
2024-08-30 02:24:33 +0530

    fixed bug

--- src/components/layout/MobileNav.tsx
+++ src/components/layout/MobileNav.tsx
@@ -95,7 +95,7 @@ export default function MobileNav({
                                 </Link>
                               ))}
                               <Link href="https://forms.gle/WfAqhAt8pp89MdCd7"><button type="button"  className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Induction_Attendance</button></Link>
-                              <button type="button" className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">T-Shirts</button>
+                          <Link href="/TShirtStore">   <button type="button" className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">T-Shirts</button></Link>
 
                             </ul>
                           </div>
