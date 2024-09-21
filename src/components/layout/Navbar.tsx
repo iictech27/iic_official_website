8ef292e824a5c072fcdc1b2f1cc48955a4ffc642
Utsav <utsavtiwari030@gmail.com>
2024-09-21 23:03:40 +0530

    added countdown page as well as promotion page

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -46,7 +46,7 @@ export default function Navbar() {
             </Link>
             </>))}
 
-          <Link href="/comingsoon"><button type="button"  className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Hackathon</button></Link>          
+          {/* <Link href="/comingsoon"><button type="button"  className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Hackathon</button></Link>           */}
         </div>
         <div className="hidden lg:flex lg:min-w-0 lg:justify-end">
           <ModeToggle />
