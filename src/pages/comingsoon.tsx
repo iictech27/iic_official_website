9178252cd426113b37829f2cf6eb93b1b6df59d8
Utsav <utsavtiwari030@gmail.com>
2024-09-22 15:29:31 +0530

    fixed code

--- src/pages/comingsoon.tsx
+++ src/pages/comingsoon.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import  FadeIn  from '@/components/animation/FadeIn'
+import Title from '@/components/core/Title'
 const comingsoon = () => {
   return (
-    <div>comingsoon</div>
+    <>
+    <Title title="COMING SOON" />
+    <FadeIn delay={0.15}>
+      <h2 className="mx-auto mt-6 max-w-3xl text-center text-2xl leading-8 text-zinc-600 dark:text-zinc-500">
+        &ldquo; The event will last 48 hours and follow a hybrid format: 8 hours online, 8 hours offline. Problem statements will be released here at 10 AM sharp on Saturday, September 28th, 2024.&rdquo;
+      </h2>
+      </FadeIn>
+</>
   )
 }
 
--- src/pages/partner.tsx
+++ src/pages/partner.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Promotion from "./promotion";
+import Title from "@/components/core/Title";
 
 const Partner = () => {
   const products = [
@@ -23,14 +24,12 @@ const Partner = () => {
   return (
     <div className="flex flex-col justify-center w-full lg:w-4/5 mx-auto">
       {/* Our Sponsors */}
-      <div>
-        <h1 className="text-4xl md:text-5xl text-center p-8 md:p-16">
-          Our Sponsors
-        </h1>
+      <div style={{paddingBottom:"-2rem"}}>
+      <Title title="Our Sponsors"/>
       </div>
 
       {/* Main section 1 */}
-      <div className="flex flex-col md:flex-row justify-between px-4 md:px-10" style={{width:"110%"}}>
+      <div className="flex flex-col md:flex-row justify-between px-4 md:px-10" style={{width:"110%", marginTop:"-5rem"}}>
         {/* Right Section */} 
         <div className="right flex flex-col md:w-2/4 px-4 md:px-8">
           {/* Logos */}
@@ -58,12 +57,12 @@ const Partner = () => {
 
           {/* Text Section */}
           <div className="mt-4 text-lg space-y-2 text-center md:text-left">
-            <p className="leading-relaxed text-2xl text-slate-700 text-center">
+            <p className="leading-relaxed text-2xl text-slate-700 text-center  dark:text-zinc-500">
               Premium, high-end, gourmet desserts,
               <br /> chocolates, and artisanal products. <br />
               Perfect for corporate gifting and bulk orders.
             </p>
-            <h2 className="mt-4 text-2xl md:text-3xl text-red-800 p-2 md:p-4 font-semibold">
+            <h2 className="mt-4 text-2xl md:text-3xl text-red-800 p-2 md:p-4 font-semibold  dark:text-zinc-500">
               Nationwide delivery available.
             </h2>
           </div>
@@ -73,7 +72,7 @@ const Partner = () => {
             <button className="bg-red-500 w-2/4 md:w-2/5 text-white py-2 md:py-1 px-4 font-semibold text-lg rounded-3xl hover:bg-red-600">
               <a href="https://linkbio.co/fivian">Connect With Us</a>
             </button>
-            <p className="text-gray-700 text-lg">
+            <p className="text-gray-700 text-lg  dark:text-zinc-500">
               Call/WhatsApp: +919088055818
             </p>
             <a
@@ -85,7 +84,7 @@ const Partner = () => {
                 alt="Instagram Icon"
                 className="w-6 h-6"
               />
-              <span className="text-lg text-gray-700">
+              <span className="text-lg text-gray-700  dark:text-zinc-500">
                 Follow us on Instagram
               </span>
             </a>
@@ -110,7 +109,7 @@ const Partner = () => {
       </div>
 
       <div className="text-center mx-auto px-4 md:px-0">
-        <p className="text-slate-700 text-lg md:text-2xl mb-8 md:mb-12">
+        <p className="text-slate-700 text-lg md:text-2xl mb-8 md:mb-12  dark:text-zinc-500">
           Fivian is a family-run confectionery brand known for its premium,
           handmade chocolates, stuffed dates, and milkshakes. Inspired by the
           number five, symbolizing family collaboration, Fivian delivers
@@ -122,7 +121,7 @@ const Partner = () => {
       {/* Product Section */}
       {/* Product Section */}
       <div className="text-center py-6 md:py-10">
-        <h1 className="text-3xl md:text-4xl font-bold mb-6 md:mb-12">
+        <h1 className="text-3xl md:text-4xl font-bold mb-6 md:mb-12  dark:text-white">
           Fivian Unique Products
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 font-semibold">
@@ -147,7 +146,7 @@ const Partner = () => {
       </div>
 
       <div className="text-center mx-auto px-4 md:px-0">
-        <p className="text-slate-700 text-lg md:text-2xl mb-8 md:mb-12">
+        <p className="text-slate-700 text-lg md:text-2xl mb-8 md:mb-12  dark:text-zinc-500">
           Fivians dedication to quality and customizable treats makes them the
           perfect partner for Skill De 2024. Their gourmet confections will
           elevate our event, offering participants a memorable experience of
